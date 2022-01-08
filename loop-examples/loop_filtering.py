c3b5ddc7b86a0f6da817d9c9c48d4853ee7991f4
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-08 23:25:53 +0000

    added loop_filtering.py

---
+++ loop-examples/loop_filtering.py
@@ -0,0 +1,16 @@
+print('Before')
+for value in [9, 41, 12, 3, 74, 15]:
+    if value > 20:
+        print('Large number',value) # 41 and 74 are > 20
+    else:
+        print('Small number',value)
+print('After')
+
+# Before
+# Small number 9
+# Large number 41
+# Small number 12
+# Small number 3
+# Large number 74
+# Small number 15
+# After
