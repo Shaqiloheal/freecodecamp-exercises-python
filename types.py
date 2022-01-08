0d378ebdf831078cc4541a57a2a9ce8b354410c9
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-08 17:03:20 +0000

    added comments

--- types.py
+++ types.py
@@ -2,6 +2,6 @@ string = ("I am a string")
 integer = (123)
 floating = (123.45)
 
-print(type(string))
-print(type(integer))
-print(type(floating))
+print(type(string)) # prints <class 'str'>
+print(type(integer)) # prints <class 'int'>
+print(type(floating)) # prints <class 'float'>
