d2e96ca2b74d17cb2328c8ab29570b90bcf69b8f
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-08 14:09:05 +0000

    update functions_basics.py
    correct typos

--- functions_basics.py
+++ functions_basics.py
@@ -1,5 +1,5 @@
 # Stored (and resued) Steps
-# def indicates the start of a function, ad the following indented section
+# def indicates the start of a function, and the following indented section
 # of code is to be stored for later.
 
 def thing():
