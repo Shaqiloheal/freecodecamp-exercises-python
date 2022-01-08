d77f77850b4bd2c1cb76696d3f6d0f72642278f6
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-08 18:28:33 +0000

    added type_conversions.py

---
+++ type_conversions.py
@@ -0,0 +1,16 @@
+print(float(99) / 100)
+# 0.99
+
+i = 42
+print(type(i))
+# <class 'int'>
+
+f = float(i)
+print(f)
+# 42.0
+
+print(type(f))
+#<class 'float'>
+
+print(1 + 2 * float(3) / 4 - 5)
+# -2.5
