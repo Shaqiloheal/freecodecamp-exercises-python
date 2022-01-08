77472b67d15598e7bda1611b43189da1d38170a0
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-08 18:39:01 +0000

    added string_conversions.py

---
+++ string_conversions.py
@@ -0,0 +1,14 @@
+sval = '123'
+print(type(sval))
+
+# print(sval + 1)
+# traceback ValueError invalid literal for int()
+
+ival = int(sval)
+print(type(ival))
+
+print(ival + 1)
+
+nsv = 'hello bob'
+# niv = int(nsv)
+# traceback ValueError invalid literal for int()
