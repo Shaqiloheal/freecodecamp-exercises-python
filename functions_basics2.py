ef1f080483db4183f953d2cca34dc233414513f7
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-08 15:07:13 +0000

    added functions_basics2.py
    understand character values with max and min functions

---
+++ functions_basics2.py
@@ -0,0 +1,5 @@
+big = max("Hello world") # lowercase w is the highest value
+print(big)
+tiny = min("Hello world") # whitespace is the lowest value
+print(tiny)
+# Uppercase is lower value than lower case letters.
