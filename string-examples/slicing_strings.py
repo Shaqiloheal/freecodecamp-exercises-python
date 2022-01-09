b19591ec054d96020d672ed06adb4b3a1b04a013
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-09 19:55:47 +0000

    update: addional examples

--- string-examples/slicing_strings.py
+++ string-examples/slicing_strings.py
@@ -7,3 +7,12 @@ print(s[6:7])
 
 print(s[6:20])
 # Python - Starts at P and ends word as last letter us n at 11.
+
+print(s[:2])
+# Mo - Assumes beginning of string and ends at o
+
+print(s[8:])
+# thon - Begins at t and continues to the end of the string.
+
+print(s[:])
+# Monthy Python - Assumes the beginning to the end of the string.
