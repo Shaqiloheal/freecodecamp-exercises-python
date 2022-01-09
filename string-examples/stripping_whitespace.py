fb2f9a4c29804253a7a4f6a7ee375085525ad2f8
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-09 22:04:53 +0000

    added stripping_whitespace.py

---
+++ string-examples/stripping_whitespace.py
@@ -0,0 +1,10 @@
+greet = '   Hello Carol  '
+
+greet.lstrip()
+# 'Hello Bob  ' - strips whitespace from left
+
+greet.rstrip()
+# '   Hello Bob' - strips whitepsace from right
+
+greet.strip()
+# 'Hello Bob' - strips beginning and ending whitespace
