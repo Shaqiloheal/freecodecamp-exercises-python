35182f72d8080f73037935ee186bdb8f3546b0eb
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-09 21:32:08 +0000

    added string_library.py

--- string-examples/string_library.py
+++ string-examples/string_library.py
@@ -13,4 +13,22 @@ stuff = 'Hello world'
 print(type(stuff))
 # <class 'str'>
 
-print(dir(stuff)
+print(dir(stuff)) # prints the capablities for the string.
+
+greet = 'Hello Warren'
+nnn = greet.upper()
+print(nnn)
+# HELLO WARREN
+
+www = greet.lower()
+print(www)
+# hello warren
+
+greet = 'Hello Robert'
+nstr = greet.replace('Robert', 'James')
+print(nstr)
+# Hello James
+
+nstr = greet.replace('r', 'X')
+print(nstr)
+# Hello XobeXt
