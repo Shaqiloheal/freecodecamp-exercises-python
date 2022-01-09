68f8f4d4c66ef16ca8363901c51bf3a65f57b9bc
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-09 20:58:54 +0000

    added string_library.py

---
+++ string-examples/string_library.py
@@ -0,0 +1,16 @@
+greet = 'Hello Carol'
+zap = greet.lower()
+print(zap)
+# hello carol
+
+print(greet)
+# Hello Carol
+
+print('Hi There'.lower())
+# hi there
+
+stuff = 'Hello world'
+print(type(stuff))
+# <class 'str'>
+
+print(dir(stuff)
