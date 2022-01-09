ebec5acbfdd506e28150c526759f37009f5c5dc6
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-09 12:50:21 +0000

    Added <string-examples> & string_data_type.py

---
+++ string-examples/string_data_type.py
@@ -0,0 +1,13 @@
+str1 = "Hello"
+str2 = 'there'
+bob = str1 + str2
+print(bob)
+# Hello there
+
+str3 = '123'
+#str3 = str3 + 1
+#Causes Traceback TypeError as it cannot concentrate str and int objects
+
+x = int(str3) + 1
+print(x)
+# 124
