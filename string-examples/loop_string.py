0501e7a4226d0f470f41815b0403b085f2254eb6
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-09 18:47:19 +0000

    update: for loop added

---
+++ string-examples/loop_string.py
@@ -0,0 +1,23 @@
+fruit = 'banana'
+index = 0
+while index < len(fruit): # 0 < 5
+    letter = fruit[index]
+    print(index, letter)
+    index = index + 1
+
+# 0 b
+# 1 a
+# 2 n
+# 3 a
+# 4 n
+# 5 a
+
+for letter in fruit:
+    print(letter)
+
+# b
+# a
+# n
+# a
+# n
+# a
