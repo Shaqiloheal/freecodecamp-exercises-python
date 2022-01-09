52c52df7a09d7a10857040844be7d773fed8dd1d
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-09 19:52:02 +0000

    added slicing_stings.py

---
+++ string-examples/slicing_strings.py
@@ -0,0 +1,9 @@
+s = 'Monty Python'
+print(s[0:4])
+# Mont - Starts from M and ends at t.
+
+print(s[6:7])
+# P - Starts from P and ends at P
+
+print(s[6:20])
+# Python - Starts at P and ends word as last letter us n at 11.
