45330fc8f25964c33164ae9f0c77cc68e14a0165
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-09 12:55:49 +0000

    update comments

--- string-examples/string_data_type.py
+++ string-examples/string_data_type.py
@@ -1,6 +1,6 @@
 str1 = "Hello"
 str2 = 'there'
-bob = str1 + str2
+bob = str1 + str2 # '+' means concentrate
 print(bob)
 # Hello there
 
@@ -8,6 +8,6 @@ str3 = '123'
 #str3 = str3 + 1
 #Causes Traceback TypeError as it cannot concentrate str and int objects
 
-x = int(str3) + 1
+x = int(str3) + 1 # converts the string into a integer
 print(x)
 # 124
