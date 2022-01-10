d54044c24b48569c3b5336b887296aa633c644d4
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-10 15:34:20 +0000

    comments added to exercise

--- python-exercises/open-read-file-program/open_read_file.py
+++ python-exercises/open-read-file-program/open_read_file.py
@@ -2,12 +2,17 @@
 # Date: 09/01/2022
 # Reads through a file called mbox-short.txt and prints the contents
 # of the file (line by line) all in upper case.
+
+# for VSC ensure to import OS
 import os
 
-fname = os.path.join(os.path.dirname(__file__), 'mbox-short.txt')
+# Use this to allow the program to open the file on terminal.
+fname = os.path.join(os.path.dirname(__file__), 'mbox-short.txt') 
 fhand = open(fname)
 
 for line in fhand:
+    # strips newlines 
     linestrip = line.rstrip()
+    # convert all characters to uppercase
     lineupper = linestrip.upper()
     print(lineupper)
