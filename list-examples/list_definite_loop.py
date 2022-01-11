6c6216868e9b76ae1c901a1cabb8b91187df445a
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-11 20:49:30 +0000

    list_definite_loop.py

---
+++ list-examples/list_definite_loop.py
@@ -0,0 +1,9 @@
+friends  = ['Warren', 'Carol', 'Dougal']
+
+for friend in friends:
+    print('Happy New Year:', friend)
+print('Done!')
+
+# for x in z:
+    # print('Happy New Year:', z)
+# print('Done!')
