664d3d9f5c9e5ce0910f1bf1523077296d4bcc0c
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-11 21:52:43 +0000

    update range_list.py

--- list-examples/range_list.py
+++ list-examples/range_list.py
@@ -1,6 +1,20 @@
 print(range(4))
+# range(0, 4)
 
 friends = ['Carol', 'Warren', 'Dougal']
 print(len(friends))
+# 3
 
 print(range(len(friends)))
+# range(0, 3)
+
+for friend in friends:
+    print('Happy New Year:', friend)
+# Happy New Year: Carol
+# Happy New Year: Warren
+# Happy New Year: Dougal
+
+# Another way of loop
+for i in range(len(friends)):
+    friend = friends[i]
+    print('Happy New Year:', friend)
