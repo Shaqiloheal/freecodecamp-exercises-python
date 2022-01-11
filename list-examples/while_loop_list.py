35af16ba9b6fc7df70d1078b4777c179ab278bf1
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-11 23:37:38 +0000

    while_loop_list.py update comments

--- list-examples/while_loop_list.py
+++ list-examples/while_loop_list.py
@@ -1,12 +1,11 @@
-# type 'done' to break out of loop.
-numlist = list()
-while True:
-    input_num = input('Enter a number: ')
-    if input_num == 'done':
-        break
-    value = float(input_num)
-    numlist.append(value)
 
-average = sum(numlist) / len(numlist)
+numlist = list() # creates a list
+while True:
+    input_num = input('Enter a number: ') # user number input
+    if input_num == 'done': # type 'done' to break out of loop.
+        break
+    value = float(input_num) #converts input into floating point number
+    numlist.append(value) # adds number to end of list
+
+average = sum(numlist) / len(numlist) # summates the list then divides the length of list
 print('Average:', average)
-5
