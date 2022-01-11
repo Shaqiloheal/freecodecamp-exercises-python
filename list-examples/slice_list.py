1f0dc1f3006110442cc235a63bef77dcdba1de33
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-11 22:44:50 +0000

    added slice_list.py

---
+++ list-examples/slice_list.py
@@ -0,0 +1,14 @@
+t = [9, 23, 12, 5, 3, 75, 15]
+
+print(t[1:3])
+# [23, 12] - up to but not including 3rd item in list
+
+print(t[:4])
+# [9, 23, 12, 5]
+
+print(t[3:])
+# [5, 3, 75, 15]
+
+print(t[:])
+# [9, 23, 12, 5, 3, 75, 15]
+
