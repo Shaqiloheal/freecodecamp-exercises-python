0e42d104984de7ec0b2315b6211538da3f6f3fdf
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-15 22:37:31 +0000

    added parse_exercise.py

---
+++ python-exercises/parse-exercise/parse_exercise.py
@@ -0,0 +1,16 @@
+# Author: Warren Spalding (Shaqiloheal)
+# Date: 16/01/2022
+
+
+import os
+
+fname = os.path.join(os.path.dirname(__file__), 'mbox-short.txt') 
+fhand = open(fname)
+
+for line in fhand:
+    line = line.rstrip()
+    words = line.split()
+    # guardian in a compound statement
+    if len(words) < 3 or if words[0] != 'From':
+        continue
+    print(words[2])
