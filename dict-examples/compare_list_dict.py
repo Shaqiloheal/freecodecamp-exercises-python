8f7626ac6aad9c987bb18d7eb30241f9ab9cafc9
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-17 19:53:49 +0000

    added compare_list_dict.py

---
+++ dict-examples/compare_list_dict.py
@@ -0,0 +1,19 @@
+lst = list()
+lst.append(21)
+lst.append(183)
+print(lst)
+# [21, 183]
+
+lst[0] = 23
+print(lst)
+# [23, 183]
+
+ddd = dict()
+ddd['age'] = 21
+ddd['course'] = 182
+print(ddd)
+# {'age': 21, 'course': 182}
+
+ddd['age'] = 23
+print(ddd)
+# {'age': 23, 'course': 182}
